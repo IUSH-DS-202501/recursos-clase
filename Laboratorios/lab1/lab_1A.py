68200b228b1a6ea8b8a29b8137a55022ce05db00
raven920 <raven@ideashock.net>
2025-03-05 08:53:11 -0500

    laboratorio1

---
+++ Laboratorios/lab1/lab_1A.py
@@ -0,0 +1,93 @@
+# Lab A
+# Nombre:
+# Cod Estudiante:  
+
+######################################  
+#                                   #  
+#             Punto #1               #  
+#                                   #  
+######################################
+
+# Escriba una función llamada primos_hasta(N) que calcule los números primos desde 0 hasta N, donde N es un parámetro de la función. Considere que N nunca va a ser un número mayor a 10000.
+# Calcule el contador de frecuencias de cada línea y el orden de magnitud del algoritmo
+
+# INICIO_SOLUCION
+# FIN_SOLUCION
+
+
+######################################  
+#                                   #  
+#             Punto #2               #  
+#                                   #  
+######################################
+
+# Escriba una función llamada potencia_mas_cercana(N,M) que calcule el exponente de la potencia de N más cercana a M, por ejemplo, potencia_mas_cercana(2,1000) devolvera 10, potencia_mas_cercana(4,40) devolverá 2 y potencia_mas_cercana(10,10000010) devolverá 7.
+# Calcule el contador de frecuencias de cada línea, el contador de frecuencias total y el orden de magnitud del algoritmo
+
+# INICIO_SOLUCION
+# FIN_SOLUCION
+
+######################################  
+#                                   #  
+#             Punto #3               #  
+#                                   #  
+######################################
+
+# Escriba una función llamada repetidos(v) donde v es un arreglo unidimensional, retorne un nuevo arreglo con los elementos que se repiten 2 o más veces dentro del arreglo.
+# Calcule el contador de frecuencias de cada línea, el contador de frecuencias total y el orden de magnitud del algoritmo
+
+# INICIO_SOLUCION
+# FIN_SOLUCION
+
+
+######################################  
+#                                   #  
+#             Punto #4               #  
+#                                   #  
+######################################
+
+# Escriba un método adicional en la clase LSL (Lista Simplemente Ligada) vista en clase llamado eliminar_pos(i), este método eliminará el elemento en la posición i de la lista. Considere que si no existe el elemento debe lanzar un error. En la medida de lo posible, use los métodos ya existentes en la clase, evite reinventar la rueda.
+# Calcule el contador de frecuencias de cada línea, el contador de frecuencias total y el orden de magnitud del algoritmo
+
+
+# INICIO_SOLUCION
+# FIN_SOLUCION
+
+######################################  
+#                                   #  
+#             Punto 5               #  
+#                                   #  
+######################################
+
+# Escriba un método adicional en la clase LSL (Lista Simplemente Ligada) vista en clase llamado insertar_pos(i,dato), este método insertara el elemento 'dato' en la posición i de la lista. Considere que si no existe la posición se debe lanzar un error, esto a menos que se trate de una lista vacia y se quiera insertar en la primera posición o que se quiera insertar al final, en caso tal se debe insertar en esas posiciones. En la medida de lo posible, use los métodos ya existentes en la clase, evite reinventar la rueda.
+# Calcule el contador de frecuencias de cada línea, el contador de frecuencias total y el orden de magnitud del algoritmo.
+
+
+# INICIO_SOLUCION
+# FIN_SOLUCION
+
+######################################  
+#                                   #  
+#             Punto 6               #  
+#                                   #  
+######################################
+
+# Escriba un método adicional en la clase LSL (Lista Simplemente Ligada) vista en clase llamada invertir(), este método invertirá la lista por completo.
+# Calcule el contador de frecuencias de cada línea, el contador de frecuencias total y el orden de magnitud del algoritmo.
+
+
+# INICIO_SOLUCION
+# FIN_SOLUCION
+
+######################################  
+#                                   #  
+#             Punto 7               #  
+#                                   #  
+######################################
+
+# Escriba un método adicional en la clase LSL (Lista Simplemente Ligada) vista en clase llamada duplicar(), este método duplicará la lista por completo poniendo el duplicado al final de la lista original.
+# Calcule el contador de frecuencias de cada línea, el contador de frecuencias total y el orden de magnitud del algoritmo.
+
+
+# INICIO_SOLUCION
+# FIN_SOLUCION
