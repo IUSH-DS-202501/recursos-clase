8826d21af334638fbdcc7cd0d5d600a5e6891719
raven920 <raven@ideashock.net>
2025-03-16 20:17:24 -0500

    Lab2

---
+++ Laboratorios/lab2/instrucciones.md
@@ -0,0 +1,49 @@
+# Lineamientos para la Entrega del Laboratorio - Curso Estructuras de Datos
+
+## Instrucciones Generales
+
+1. **Desarrollo del Algoritmo**:
+   - Cada laboratorio está compuesto por varios puntos.
+   - En cada punto, deberás desarrollar un algoritmo en el lenguaje de programación de tu preferencia.
+   - Asegúrate de que tu solución cumpla con lo especificado en el enunciado, no más, no menos.
+
+2. **Archivos del Laboratorio**:
+   - Para este laboratorio solo está disponible el archivo `lab_2A`.
+   - El archivo contiene los puntos a desarrollar.
+
+3. **Formato de la Solución**:
+   - Para los puntos en los que se solicita calcular el contador de frecuencias, se debe hacer por medio de comentario al final de la línea según lo visto en clase, al final del algoritmo se debe poner un comentario adicional con el contador de frecuencias total.
+   - Para los puntos en los que se solicita calcular el orden de magnitud, se debe poner un comentario adicional al final del agoritmo  con el orden de magnitud en notación BigO segun lo visto en clase.
+   - Cada punto tiene unas instrucciones claras de que es lo que se espera, siga las instrucciones detalladamente, especial atención al nombramiento de los métodos y atributos. Si se solicita que se llamen por ejemplo 'mover', no será valido ningun otro nombre, ni 'moverValor', 'moviendo', 'moverA'. La atención al detalle es determinante para aprobar. 
+   - Cada punto en el archivo tiene una sección que indica el inicio y el final de la solución llamados `INICIO_SOLUCION` y `FIN_SOLUCION`, estos deben estar tal cual como se especifica, de lo contrario la calificación será negativa.
+   - Debes colocar tu solución **únicamente dentro de ese espacio**.
+   - La solución debe ser completa e independiente de las demás, si se importan bibliotecas externas, estos imports deben formar parte de cada punto que son usados.
+   - Si usas Python, por favor valida la correcta identación del código antes de entregarlo, de lo contrario se calificará como incorrecto.
+   - No modifiques ni elimines ninguna otra parte del archivo.
+   - El no seguir las instrucciones anteriores causará la pérdida del entregable de manera irremediable.
+
+## Envío del Archivo
+
+1. **Formato y Nombre del Archivo**:
+   - El archivo debe ser enviado en **formato plano** (puede ser .txt, .cpp, .py, etc.).
+   - Es crucial que **conserves el nombre original** del archivo (`lab_2A` ).
+   - El no seguir estas instrucciones implicará que la entrega es inválida.
+
+2. **Método de Envío**:
+   - Debes enviar el archivo a nuestro chat directo en Teams.
+
+3. **Verificación del Documento**:
+   - Asegúrate de que el archivo enviado esté correctamente y que contenga tu solución dentro del espacio indicado.
+
+4. **Fecha máxima de envío:**:
+   - 24 de Marzo del 2025 a las 23:59:59 PM
+
+## Evaluación
+
+- La evaluación del laboratorio es automatizada.
+- La realización y entrega del laboratorio es INDIVIDUAL.
+- Cualquier incumplimiento con los lineamientos anteriores resultará en la invalidez de la entrega.
+- El código será ejecutado y la sintaxis validada, si no ejecuta, el punto se dará por perdido.
+- Para aprobar debes tener al menos la mitad + 1 de los puntos correctos.
+
+Por favor, asegúrate de seguir todas las instrucciones cuidadosamente para garantizar que tu entrega sea válida y evaluada correctamente.
---
+++ Laboratorios/lab2/lab_2A.py
@@ -0,0 +1,78 @@
+# Lab A
+# Nombre:
+# Cod Estudiante:  
+
+######################################  
+#                                   #  
+#             Punto #1               #  
+#                                   #  
+######################################
+
+# Las Listas Simplemente Ligadas Circulares son listas donde el último elemento apunta al primero con su liga. Haga los cambios necesarios en la clase Lista Simplemente Enlazada vista en clase para soportar esta característica. Note que si la lista está vacía el primer y último elemento siguen siendo nulos. Incluya todo el código de la declaración de la clase Lista Ligada Circular y ejemplos del uso de los métodos.
+
+# INICIO_SOLUCION
+# FIN_SOLUCION
+
+
+######################################  
+#                                   #  
+#             Punto #2               #  
+#                                   #  
+######################################
+
+# Cree un método llamado concatenar(lista) de la Lista Simplemente Circular donde se reciba otra Lista Simplemente Circular y se agregue al final de la lista en la menor cantidad de operaciones posibles. Incluya todo el código de la declaración de la clase Lista Ligada Circular una vez más y un ejemplo del funcionamiento del método.
+
+# INICIO_SOLUCION
+# FIN_SOLUCION
+
+######################################  
+#                                   #  
+#             Punto #3               #  
+#                                   #  
+######################################
+
+# Implemente un método llamado rotarIzquierda(...) dentro de la clase Cola que rote los elementos de la lista una posición a la izquierda. Incluya todo el código de la declaración de la clase Cola y un ejemplo del funcionamiento del método.
+
+
+# INICIO_SOLUCION
+# FIN_SOLUCION
+
+
+######################################  
+#                                   #  
+#             Punto #4               #  
+#                                   #  
+######################################
+
+# Partiendo de la clase Pila vista en clase, cree una clase llamada ColaDosLados, donde todos los elementos son apilados en la parte superior, pero al desapilar, elimine elementos de la parte superior y de la parte inferior de la Pila de manera intercalada. Incluya todo el código de la declaración de la clase ColaDosLados y un ejemplo del funcionamiento del método.
+
+
+# INICIO_SOLUCION
+# FIN_SOLUCION
+
+######################################  
+#                                   #  
+#             Punto 5               #  
+#                                   #  
+######################################
+
+# Cree una clase llamada GestorProductos donde implemente los métodos para hacer la gestión de productos de una empresa, la clase deberá leer un archivo JSON con una estructura que contenga información de productos de la siguiente manera:
+
+# [
+#     {
+#         "Producto": "Leche",
+#         "Precio": 5000,
+#         "Existencia": 25
+#     },
+#     {
+#         "Producto": "Leche",
+#         "Precio": 5000,
+#         "Existencia": 25
+#     }
+# ]
+
+# Su clase deberá almacenar los productos en un HashMap, de tal forma que se pueda obtener de manera eficiente en orden de magnitud constante, para esto use un HashMap, haga uso de las clases vistas en el aula. Incluya todo el código de la declaración de la clase GestorProductos y un ejemplo del funcionamiento de los métodos relevantes. Incluya por lo menos: agregarProducto(...), eliminarProducto(...), actualizarExistencias(...), actualizarPrecio(...).
+
+
+# INICIO_SOLUCION
+# FIN_SOLUCION
