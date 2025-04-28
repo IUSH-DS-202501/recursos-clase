f7c7808f2dff71e9f2fdda6ca693ae42642bee6d
raven920 <raven@ideashock.net>
2025-04-28 04:12:20 -0500

    Lab3

---
+++ Laboratorios/lab3/lab_3A.py
@@ -0,0 +1,22 @@
+# Lab A
+# Nombre:
+# Cod Estudiante:  
+
+######################################  
+#                                   #  
+#             Punto #1               #  
+#                                   #  
+######################################
+
+# Dada una lista de nombres o IDs de Pokémon, se debe obtener información de cada uno consultando la PokéAPI (https://pokeapi.co/). Para cada Pokémon, identificar su tipo principal (considerado como el tipo asociado al slot: 1 en los datos de la API). Finalmente, organizar y representar esta información mediante una estructura de datos jerárquica (un árbol), donde los Pokémon se agrupen bajo nodos que representen su tipo principal correspondiente. Cree la clase NodoPoke y la clase ArbolPoke donde almacene la información, brinde algunos ejemplos de la ejecución.
+
+# INICIO_SOLUCION
+# FIN_SOLUCION
+
+######################################  
+#                                    #  
+#             Punto #2               #  
+#                                    #  
+######################################
+
+# Diseñe e implemente un programa que simule un torneo Pokémon de eliminación directa para N participantes, donde N siempre será una potencia de 2, utilizando un árbol binario para modelar los brackets: las hojas representarán a los entrenadores iniciales y cada nodo interno una batalla entre los ganadores de sus hijos. Implementa una lógica que reciba la lista de los entrenadores y sus pokemones, y una forma de simular cada batalla, determina el ganador basándote en una regla predefinida y, mediante un recorrido apropiado del árbol, simula las rondas del torneo desde las batallas iniciales hasta la final en la raíz, reportando finalmente al entrenador campeón. Cree la clase NodoEntrenador y la clase ArbolTorneo donde almacene la información, brinde algunos ejemplos de la ejecución.
